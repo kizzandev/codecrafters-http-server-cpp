6aeb2f8965f74099edb1b5afe28d9eb24d3d964a
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-04 16:55:31 -0300

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -11,7 +11,7 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> split(const std::string &s, const char delim) {
+std::vector<std::string> split(const std::string &s, char delim) {
   std::vector<std::string> elems;
   std::stringstream ss(s);
   std::string item;
