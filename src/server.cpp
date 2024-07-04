a54a7feb32be17221faa14b789be9fbacaac5f13
kizzandev <69643444+kizzandev@users.noreply.github.com>
2024-07-04 17:00:07 -0300

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -15,7 +15,7 @@ std::vector<std::string> split(const std::string &s, const char *delim) {
   std::vector<std::string> elems;
   std::stringstream ss(s);
   std::string item;
-  while (std::getline(ss, item, delim)) {
+  while (std::getline(ss, item, *delim)) {
     elems.push_back(item);
   }
   return elems;
